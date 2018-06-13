a7c1462ef14efe52f00f3ac40576e56273f02f10
ASUS-T84KKB6\Diego <diegolmontano@gmail.com>
2018-06-12 23:20:51 -0500

    Added improved permissions to use profile feed items API.

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -13,3 +13,17 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+class PostOwnStatus(permissions.BasePermission):
+    '''
+    Allow users to update their own statusself.
+    '''
+    def has_object_permission(self, request, view, obj):
+        '''
+        Checks if the user is trying to update its own status.
+        '''
+        if request.method in permissions.SAFE_METHODS:
+
+            return True
+
+        return obj.user_profile.id == request.user.id
--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -5,8 +5,10 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from . import models
 from . import permissions
 from . import serializers
@@ -146,3 +148,18 @@ class LoginViewSet(viewsets.ViewSet):
         Use the ObtainAuthToken APIView to validate and create a token.
         '''
         return ObtainAuthToken().post(request)
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    '''
+    Handles creating, reading and updating profile feed items.
+    '''
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (permissions.PostOwnStatus, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        '''
+        Sets the user profile to the logged in user.
+        '''
+        serializer.save(user_profile=self.request.user)
